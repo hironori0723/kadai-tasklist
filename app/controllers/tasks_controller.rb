fd819bf3513c675e091fce6e4bfb7ecb540fbd46
hironori.suzuki <604849hiro.s@gmail.com>
2019-09-17 14:09:14 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
  before_action :set_task, only: [:show, :edit, :update, :destroy]
  before_action :require_user_logged_in
- before_action :correct_user, only: [:destroy]
+ before_action :correct_user, only: [:update, :destroy]
  
  def index
      @tasks = Task.all.page(params[:page]).per(10)
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
  end
  
  def update
-     
+     @task = current_user.tasks.build(task_params)
      if @task.update(task_params)
          flash[:success] = 'Taskの変更を保存しました'
          redirect_to @task
@@ -47,20 +47,20 @@ class TasksController < ApplicationController
  
 private
 
- def correct_user
-     @task = current_user.tasks.find_by(id: params[:id])
-     unless @task
-     redirect_to root_url
-     end
- end    
-
  def set_task
      @task = Task.find(params[:id])
  end
 
 # Strong Parameter
  def task_params
-     params.require(:task).permit(:content, :status)
+     params.require(:task).permit(:content, :status, :id)
  end
 
+ def correct_user
+     @task = current_user.tasks.find_by(id: params[:id])
+     unless @task
+     redirect_to root_url
+     end
+ end    
+     
 end
